2bd3648abb0715abe7f924984aaacccbb9e76225
Martha1981 <marthacecilia3612@hotmail.com>
2022-08-11 20:44:17 -0400

    Primer commit

---
+++ src/main/java/Clases/ClsUsuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author PC
+ */
+public class ClsUsuario {
+
+    String numeroDocumento;
+    String nombre;
+    String apellido;
+    String privilegio;
+
+}
+  
